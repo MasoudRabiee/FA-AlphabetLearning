4c8769a5c50afcc52eab72826e9179ee0e922325
MasoudRabiee <masoud.rabiee20@gmail.com>
2021-10-31 16:52:06 +0330

    Coding MainActivity

--- app/src/main/java/com/example/alphabetlearning/view/MainActivity.kt
+++ app/src/main/java/com/example/alphabetlearning/view/MainActivity.kt
@@ -1,12 +1,30 @@
 package com.example.alphabetlearning.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.alphabetlearning.R
+import com.example.alphabetlearning.data.DataAlphabet
+import com.example.alphabetlearning.databinding.ActivityMainBinding
+import com.example.alphabetlearning.model.Alphabet
+import com.example.alphabetlearning.model.AlphabetLetter
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var farsiAlphabet : Alphabet
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        farsiAlphabet = DataAlphabet.farsiAlphabet
+        binding.btnDrawLetter.setOnClickListener {
+            val secondActivity = Intent(this , LetterActivity::class.java)
+            secondActivity.putExtra("alphabet", farsiAlphabet)
+            startActivity(secondActivity)
+        }
     }
 }
