3d542806645dad23955d2dabf5b82fcb7c954e2a
MasoudRabiee <masoud.rabiee20@gmail.com>
2021-11-20 17:21:09 +0330

    Add BlankRecyclerAdapter.kt

---
+++ app/src/main/java/com/example/alphabetlearning/adapter/BlankRecyclerAdapter.kt
@@ -0,0 +1,24 @@
+package com.example.alphabetlearning.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class BlankRecyclerAdapter() : RecyclerView.Adapter<BlankRecyclerAdapter.BHolder>() {
+
+    inner class BHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: BHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
