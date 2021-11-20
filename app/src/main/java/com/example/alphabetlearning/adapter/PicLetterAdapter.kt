51d919fd728a714b5b0a92932c38910b0fe2d200
MasoudRabiee <masoud.rabiee20@gmail.com>
2021-11-20 18:03:08 +0330

    Working on PicLetterAdapter.kt and Add xml files

--- app/src/main/java/com/example/alphabetlearning/adapter/PicLetterAdapter.kt
+++ app/src/main/java/com/example/alphabetlearning/adapter/PicLetterAdapter.kt
@@ -1,24 +1,66 @@
 package com.example.alphabetlearning.adapter
 
+import android.content.ClipData
+import android.content.ClipDescription
+import android.content.Context
+import android.os.Build
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.alphabetlearning.R
+import com.example.alphabetlearning.model.LetterTranslator
 
-class PicLetterAdapter() : RecyclerView.Adapter<PicLetterAdapter.PicHolder>() {
+class PicLetterAdapter(private val _context: Context, private val _listData: Array<LetterTranslator>) : RecyclerView.Adapter<PicLetterAdapter.PicHolder>() {
 
-    inner class PicHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+    inner class PicHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val image = itemView.findViewById<ImageView>(R.id.image_drag)
 
+
+        fun bindHolder(context: Context, listData: LetterTranslator) {
+            val maskDragMessage = listData.letter.toString()
+            val resourceId = context.resources.getIdentifier(
+                listData.letterImage,
+                "drawable",
+                context.packageName
+            )
+            image.setImageResource(resourceId)
+            image.setOnLongClickListener {
+                val item = ClipData.Item(maskDragMessage)
+
+                val dataToDrag = ClipData(
+                    maskDragMessage,
+                    arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN),
+                    item
+                )
+                val maskShadow = MaskDragShadowBuilder(it, listData, context)
+
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+                    //support pre-Nougat versions
+                    @Suppress("DEPRECATION")
+                    it.startDrag(dataToDrag, maskShadow, it, 0)
+                } else {
+                    //supports Nougat and beyond
+                    it.startDragAndDrop(dataToDrag, maskShadow, it, 0)
+                }
+
+                it.visibility = View.INVISIBLE
+                true
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PicHolder {
-        TODO("Not yet implemented")
+        val view = LayoutInflater.from(_context).inflate(R.layout.letter, parent, false)
+        return PicHolder(view)
     }
 
     override fun onBindViewHolder(holder: PicHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bindHolder(_context, _listData[position])
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return _listData.count()
     }
 }
