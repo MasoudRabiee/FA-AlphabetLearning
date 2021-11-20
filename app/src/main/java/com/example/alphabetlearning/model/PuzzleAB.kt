c3c0a7aad9236d5b93070c9049633263c51f9732
MasoudRabiee <masoud.rabiee20@gmail.com>
2021-11-20 18:37:09 +0330

    Add splitName function

--- app/src/main/java/com/example/alphabetlearning/model/PuzzleAB.kt
+++ app/src/main/java/com/example/alphabetlearning/model/PuzzleAB.kt
@@ -1,4 +1,10 @@
 package com.example.alphabetlearning.model
 
 class PuzzleAB(var name : String , var imagePuzzle: String) {
+
+    fun splitName() : Array<Char>{
+        return Array<Char>(name.length){
+            name[it]
+        }
+    }
 }
