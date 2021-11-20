f04c961b43a9d705a5a08fbd6621908a49fa32e8
MasoudRabiee <masoud.rabiee20@gmail.com>
2021-11-20 18:33:21 +0330

    Add DataPuzzle.kt

---
+++ app/src/main/java/com/example/alphabetlearning/data/DataPuzzle.kt
@@ -0,0 +1,8 @@
+package com.example.alphabetlearning.data
+
+import com.example.alphabetlearning.model.PuzzleAB
+
+object DataPuzzle {
+    // template data :
+    val temp = PuzzleAB("water"  , "آب")
+}
