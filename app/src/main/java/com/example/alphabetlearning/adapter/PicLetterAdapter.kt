db74262e493ca3de808c562073cad723033f3bdc
MasoudRabiee <masoud.rabiee20@gmail.com>
2021-11-20 17:25:01 +0330

    Add PicLetterAdapter.kt

---
+++ app/src/main/java/com/example/alphabetlearning/adapter/PicLetterAdapter.kt
@@ -0,0 +1,24 @@
+package com.example.alphabetlearning.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class PicLetterAdapter() : RecyclerView.Adapter<PicLetterAdapter.PicHolder>() {
+
+    inner class PicHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PicHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: PicHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
