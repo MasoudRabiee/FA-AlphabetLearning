fb58ee3c697cfea23313c4c1ef41797b37dc246c
MasoudRabiee <masoud.rabiee20@gmail.com>
2021-11-24 17:52:16 +0330

    Add data for PuzzleActivity

--- app/src/main/java/com/example/alphabetlearning/data/DataPuzzle.kt
+++ app/src/main/java/com/example/alphabetlearning/data/DataPuzzle.kt
@@ -3,8 +3,6 @@ package com.example.alphabetlearning.data
 import com.example.alphabetlearning.model.PuzzleAB
 
 object DataPuzzle {
-    // template data :
-    val temp = PuzzleAB("water"  , "آب")
     val data = arrayListOf<PuzzleAB>(
         PuzzleAB("water"  , "آب"),
         PuzzleAB("darya"  , "دریا"),
