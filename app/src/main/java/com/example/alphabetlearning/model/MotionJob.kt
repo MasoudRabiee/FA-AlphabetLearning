0e736c5ca6d28c5f573ab570bc16717d04236219
MasoudRabiee <masoud.rabiee20@gmail.com>
2021-12-07 18:26:33 +0330

    Add MotionJob.kt class

---
+++ app/src/main/java/com/example/alphabetlearning/model/MotionJob.kt
@@ -0,0 +1,43 @@
+package com.example.alphabetlearning.model
+
+import android.widget.Button
+import kotlinx.coroutines.*
+
+class MotionJob(private var btn: Button) {
+
+    companion object {
+        private var screenWidth: Int = 0
+        private var screenHeight: Int = 0
+
+        fun configure(width: Int, height: Int) {
+            screenWidth = width
+            screenHeight = height
+        }
+    }
+
+    var jobID: Int = btn.id
+    var job: Job = Job()
+
+
+    fun launchJob() {
+        job = GlobalScope.launch(Dispatchers.Main) {
+            var xIndex = 1
+            var yIndex = 1
+            while (true) {
+                when {
+                    btn.x > screenWidth - btn.width -> xIndex = -1
+                    btn.x < 0 -> xIndex = 1
+                    btn.y > screenHeight - btn.height -> yIndex = -1
+                    btn.y < 0 -> yIndex = 1
+                }
+                btn.x += xIndex
+                btn.y += yIndex
+                delay(1)
+            }
+        }
+    }
+
+    fun stopJob() {
+        this.job.cancel()
+    }
+}
