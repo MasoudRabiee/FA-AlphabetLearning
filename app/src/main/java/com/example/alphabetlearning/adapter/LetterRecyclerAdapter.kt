87ae3d16021066091e7fdeffdb629d03eb6ce13e
MasoudRabiee <masoud.rabiee20@gmail.com>
2021-12-11 19:52:31 +0330

    Add new field & Editing LetterRecyclerAdapter.kt

--- app/src/main/java/com/example/alphabetlearning/adapter/LetterRecyclerAdapter.kt
+++ app/src/main/java/com/example/alphabetlearning/adapter/LetterRecyclerAdapter.kt
@@ -10,22 +10,34 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.alphabetlearning.R
 import com.example.alphabetlearning.model.AlphabetLetter
 import com.example.alphabetlearning.view.DrawingActivity
+import com.example.alphabetlearning.view.MotionGameActivity
 
 class LetterRecyclerAdapter(
     private val _context: Context,
-    private val _letterList: ArrayList<AlphabetLetter>
+    private val _letterList: ArrayList<AlphabetLetter>,
+    private val option : Int
 ) : RecyclerView.Adapter<LetterRecyclerAdapter.Holder>() {
 
     inner class Holder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         private val imageCategory = itemView.findViewById<ImageView>(R.id.image_view1)
 
-        fun bindHolder(context: Context, letter:AlphabetLetter): Unit {
+        fun bindHolder(context: Context, letter:AlphabetLetter , setClickKind : Int) {
             val resourceId = context.resources.getIdentifier(letter.imageName , "drawable" , context.packageName)
             imageCategory.setImageResource(resourceId)
-            imageCategory.setOnClickListener{
-                val nextActivity = Intent(_context , DrawingActivity::class.java)
-                nextActivity.putExtra("shape" , letter)
-                _context.startActivity(nextActivity)
+            if (setClickKind == 1){
+                imageCategory.setOnClickListener {
+                    val nextActivity = Intent(context , MotionGameActivity::class.java).apply {
+                        putExtra("id" , letter.letter)
+                    }
+                    context.startActivity(nextActivity)
+                }
+            }
+            else {
+                imageCategory.setOnClickListener{
+                    val nextActivity = Intent(context , DrawingActivity::class.java)
+                    nextActivity.putExtra("shape" , letter)
+                    context.startActivity(nextActivity)
+                }
             }
         }
     }
@@ -36,7 +48,7 @@ class LetterRecyclerAdapter(
     }
 
     override fun onBindViewHolder(holder: Holder, position: Int) {
-        holder.bindHolder(_context , _letterList[position])
+        holder.bindHolder(_context , _letterList[position] , option)
     }
 
     override fun getItemCount(): Int {
