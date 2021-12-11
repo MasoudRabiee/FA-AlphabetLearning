77bf2a9b8b296f27eb1d15b4986d8f123b46dd28
MasoudRabiee <masoud.rabiee20@gmail.com>
2021-12-11 19:48:32 +0330

    Delete jpg and Add new data Object

---
+++ app/src/main/java/com/example/alphabetlearning/data/DataPersianLetter.kt
@@ -0,0 +1,9 @@
+package com.example.alphabetlearning.data
+
+object DataPersianLetter {
+    val LETTER = arrayListOf<String>(
+       "آ" , "ب" , "پ","ت","ث","ج","چ","ح","خ","د","ذ","ر",
+        "ز","ژ","س","ش","ص","ض","ط","ظ","ع","غ","ف","ق","ک","گ","ل",
+        "م","ن","و","ه","ی"
+    )
+}
