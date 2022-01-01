7edd79d357ffe0253a0934f4aa0bca839d38d307
MasoudRabiee <masoud.rabiee20@gmail.com>
2022-01-01 20:06:26 +0330

    useless import deleted

--- app/src/main/java/com/example/alphabetlearning/view/MainActivity.kt
+++ app/src/main/java/com/example/alphabetlearning/view/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.DisplayMetrics
-import com.example.alphabetlearning.data.DataSkeleton
 import com.example.alphabetlearning.databinding.ActivityMainBinding
 import com.example.alphabetlearning.model.ScreenTool
 
